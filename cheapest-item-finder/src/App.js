c16540849affd3e4dcb275bb428e952cbf867177
eSpyd3r <ethan.lim@gmail.com>
2023-12-31 15:10:54 -0500

    implemented template for homepage

--- cheapest-item-finder/src/App.js
+++ cheapest-item-finder/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Cheapest Item Finder</h1>
       </header>
+      <div className="Search-area">
+        <input type="text" placeholder="Search for an item..." className="Search-input"/>
+        <button className="Search-button">Search</button>
+      </div>
+      <footer className="App-footer">
+        <p>© 2023 Cheapest Item Finder</p>
+      </footer>
     </div>
   );
 }
